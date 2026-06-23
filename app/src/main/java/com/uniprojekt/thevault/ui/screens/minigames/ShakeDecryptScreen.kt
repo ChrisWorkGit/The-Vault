148e6c6a388134ea83ae06ed555e1e1d677e403b
Maverick Haas <191795490+mav42@users.noreply.github.com>
2026-06-23 17:05:24 +0200

    create ShakeDecryptScreen file
    Add description of the minigame content/gameplay.

---
+++ app/src/main/java/com/uniprojekt/thevault/ui/screens/minigames/ShakeDecryptScreen.kt
@@ -0,0 +1,16 @@
+package com.uniprojekt.thevault.ui.screens.minigames
+
+import androidx.compose.runtime.Composable
+
+/**
+ * Minispiel "ShakeDecrypt".
+ *
+ * Jeder Spieler bekommt einen Teil des Tresorcodes, der zu Beginn "verschlüsselt" dargestellt wird.
+ * Durch Schütteln des Handys (erkannt über Beschleunigungssensor) steigt der Fortschritt, der nach und nach die echten Ziffern aufdeckt.
+ * Je schneller/öfter geschüttelt wird, desto schneller ist der Code vollständig "entschlüsselt".
+ * ACHTUNG: Diese Implementierung ist für ein einzelnes Gerät. Eine echte Mehrspielerfunktionalität ist hier noch nicht enthalten.
+ */
+@Composable
+fun ShakeDecryptScreen() {
+
+}
